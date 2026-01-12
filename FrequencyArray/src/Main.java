57e9b96a407ed6b2c41b4e89dbe04a132b4f269f
enesarikk <enesarik2093@gmail.com>
2026-01-12 15:45:16 +0300

    Arrays

--- FrequencyArray/src/Main.java
+++ FrequencyArray/src/Main.java
@@ -1,8 +1,26 @@
-import java.util.Arrays;
-
 public class Main {
+    static boolean frequency(int[] array, int value){
+        for(int s : array){
+            if(s == value) return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] seq = {10, 20, 20, 10, 10, 20, 5, 20};
-        System.out.println("Array : " + Arrays.toString(seq));
+        int[] copy = new int[seq.length];
+
+        for(int i = 0; i < seq.length; i++){
+                if(!frequency(copy,seq[i])){
+                    int counter = 0;
+                    for(int number : seq){
+                        if(seq[i] == number){
+                            counter++;
+                        }
+                }
+                System.out.println(seq[i] + " repeated " + counter + " times.");
+                copy[i] = seq[i];
+            }
+        }
     }
 }
